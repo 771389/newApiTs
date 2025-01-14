d3e77f93adc9d0f9d9258e50ccc4f1a6e200f2df
Clube dos Mods <vitormanoel584@gmail.com>
2025-01-14 20:44:39 -0300

    newservers2

--- server.js
+++ server.js
@@ -13,8 +13,11 @@ const canais1 = require('./routes/84.16.253.11.json');
 const cineprime = require('./routes/178.162.197.177.json');
 const pegasus = require('./routes/pegasus.tvvip.live.json');
 const categorias = require('./routes/categorias.json');
+const cebola = require('./routes/dns.cebola.pro');
 const authxplus = require('./routes/authxplus-2.xyz.json');
-
+const uniplay = require('./routes/uniplayuhd.cdn23.click.json');
+const vipy = require('./routes/vipy.pro.json');
+const cebola = require('./routes/dns.cebola.pro.json');
 
 // Middleware para interpretar o corpo das requisições como JSON
 app.use(express.json());  // Adicionado para lidar com dados JSON
@@ -56,11 +59,42 @@ app.get('/routes/178.162.197.177', (req, res) => res.json(cineprime));
 app.get('/routes/pegasus.tvvip.live', (req, res) => res.json(pegasus));
 
 
-// Rota para retornar o arquivo pegasus.json
+// Rota para retornar o arquivo categorias.json
 app.get('/routes/categorias', (req, res) => res.json(categorias));
 
-// Rota para retornar o arquivo pegasus.json
+// Rota para retornar o arquivo authplus.json
 app.get('/routes/authxplus-2.xyz', (req, res) => res.json(authxplus));
+
+
+
+
+
+
+
+// Rota para retornar o arquivo dns.cebola.pro json
+app.get('/routes/dns.cebola.pro', (req, res) => res.json(cebola));
+
+// Rota para retornar o arquivo uniplayuhd.cdn23.click json
+app.get('/routes/uniplayuhd.cdn23.click', (req, res) => res.json(uniplay));
+
+// Rota para retornar o arquivo vip.pro json
+app.get('/routes/vipy.pro', (req, res) => res.json(vipy));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Inicia o servidor
